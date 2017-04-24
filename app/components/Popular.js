e77dd36f869e9a05afa981fc9ffe118dddf16e89
chainsawbucktooth <chainsaw.bucktooth@gmail.com>
2017-04-24 10:16:44 -0400

    created stateless function for SelectedLanguage horizontal bar

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,48 +1,62 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+
+function SelectLanguage(props)  {
+  var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+  return (
+    <ul className='languages'>
+      {languages.map(function(lang){
+          return (
+            <li
+              style= {lang === props.selectedLanguage ? { color: '#d0021b'}: null }
+              onClick = {props.updateLanguage.bind(null, lang)}
+              key={lang}>
+              {lang}
+            </li>
+          )
+      })}
+    </ul>
+  )
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.string.isRequired,
+}
 
 
 class Popular extends React.Component {
 
   //this constructor is how we create STATE, which we use to keep track of which language is currently selected
-  constructor (props) {
-     super(props);
+  constructor(props) {
+     super();
      this.state = {
-        selectedLanguage: 'All'
+        selectedLanguage: 'All',
      };
 
     //bind to set the context of the function, can also use it to pass argument
     //bind property takes in a context and returns a new function with this bound to whatever you pass it
     //updateLanugage will always be called with the correct context
-    this.updateLanguage = this.updateLanguage.bind(this)
-
+    this.updateLanguage = this.updateLanguage.bind(this);
   }
 
   //this is the function that will update STATE
   updateLanguage(lang) {
-    this.setState(function(){
+    this.setState(function() {
       return {
-        selectedLanguage: lang
+        selectedLanguage: lang,
       }
-    })
+    });
   }
 
-
-
   render() {
-    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
     return (
-      <ul className='languages'>
-        {languages.map(function(lang){
-            return (
-              <li
-                style= {lang === this.state.selectedLanguage ? { color: '#d0021b'}: null }
-                onClick = {this.updateLanguage.bind(null, lang)}
-                key={lang}>
-                {lang}
-              </li>
-            )
-        }, this)}
-      </ul>
+      <div>
+        <SelectLanguage
+         selectedLanguage = {this.state.selectedLanguage}
+         onSelect = {this.updateLanguage} />
+      </div>
     )
   }
 }
