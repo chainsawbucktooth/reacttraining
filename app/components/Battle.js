56b7c4c9062a4585086b913cef838b70e67aa117
chainsawbucktooth <chainsaw.bucktooth@gmail.com>
2017-04-24 16:09:01 -0400

    created Switch for NavBar

---
+++ app/components/Battle.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+class Battle extends React.Component {
+  render() {
+    return (
+      <div>
+        BATTLE TO THE DEATH!!!
+      </div>
+    )
+  }
+}
+
+
+module.exports = Battle;
